a3af276e55a2c22cbf7c9b14231a843813bf67f0
Berat Deniz <70016159+BeratDen@users.noreply.github.com>
2023-09-02 22:02:28 +0300

    fix for css file you should never import them from your local directory

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,13 +1,13 @@
-import Footer from "@/Pages/MainPagesFolder/Footer"
-import Nav from "@/Pages/MainPagesFolder/Navbar"
+import Footer from "@/Pages/MainPagesFolder/Footer";
+import Nav from "@/Pages/MainPagesFolder/Navbar";
+import "@/../css/footer.css";
+import "@/../css/home.css";
 
-export default function Layout({children}){
-    return(
+export default function Layout({ children }) {
+    return (
         <div>
-        {children}
-        <Footer/>
-
+            {children}
+            <Footer />
         </div>
-    )
-
+    );
 }
